20bba67b690a3e69ecc7242efb2e11f28af4cc62
Onu <onucsecu@gmail.com>
2023-10-29 01:34:50 +0900

    add registration functionality

---
+++ codejavu/custom_auth/serializers.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    refresh_token = serializers.CharField(required=False)
+    last_login = serializers.DateTimeField(required=False)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+        extra_kwargs = {
+            'refresh_token': {'required': True},
+            'last_login': {'required': True}
+        }
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `User` instance refresh_token and last_login time
+        """
+        instance.refresh_token = validated_data.get('refresh_token', instance.refresh_token)
+        instance.last_login = validated_data.get('last_login', instance.last_login)
+        instance.save()
+        return instance
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+        extra_kwargs = {
+            'email': {'required': True},
+            'password': {'required': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            email=validated_data['email'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- codejavu/custom_auth/views.py
+++ codejavu/custom_auth/views.py
@@ -1,10 +1,14 @@
+from datetime import datetime
+
 from django.contrib.auth import get_user_model
+from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import exceptions
 from .auth import generate_access_token
+from .serializers import RegisterSerializer, LoginSerializer
 
 
 @api_view(['POST'])
@@ -14,7 +18,6 @@ def login_view(request):
     user_auth_model = get_user_model()
     email = request.data.get('email')
     password = request.data.get('password')
-    response = Response()
     if (email is None) or (password is None):
         raise exceptions.AuthenticationFailed(
             'email and password required')
@@ -24,9 +27,28 @@ def login_view(request):
         raise exceptions.AuthenticationFailed('user not found')
     if not user.check_password(password):
         raise exceptions.AuthenticationFailed('wrong password')
-
     token = generate_access_token(user)
-    refresh_token = token['refresh']
-    response.set_cookie(key='refresh_token', value=refresh_token, httponly=True)
-    response.data = token
-    return response
+    refresh_token = token["refresh"]
+
+    headers = {
+        "token": token["access"]
+    }
+    data = {
+        "refresh_token": refresh_token,
+        "last_login": datetime.now()
+    }
+    serializer = LoginSerializer(data=data)
+    serializer.update(instance=user, validated_data=data)
+    return Response(headers=headers)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+@ensure_csrf_cookie
+def registration_view(request):
+    data = request.data
+    serializer = RegisterSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
