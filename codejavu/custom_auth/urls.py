efc15492c6212ec1b600bc2c741642751209c78b
Onu <onucsecu@gmail.com>
2024-03-06 14:32:12 +0900

    rename auth route url

--- codejavu/custom_auth/urls.py
+++ codejavu/custom_auth/urls.py
@@ -4,7 +4,7 @@ from .views import login_view, registration_view, token_refresh_view, token_blac
 
 urlpatterns = [
     path('login', login_view, name='login'),
-    path('registration', registration_view, name='registration'),
+    path('signup', registration_view, name='registration'),
     path('refresh', token_refresh_view, name='token_refresh'),
     path('logout', token_blacklist_view, name='token_logout'),
 ]
